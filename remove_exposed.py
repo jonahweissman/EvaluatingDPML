bdd1bc5cabab16d13aec5034823ad50ba2e76188
Jonah Weissman <jw5av@virginia.edu>
2019-07-17 12:28:35 -0400

    Add yeom support and file writing

--- remove_exposed.py
+++ remove_exposed.py
@@ -1,21 +1,20 @@
-from attack import load_data, attack_experiment
-from classifier import get_predictions, train_private
+from attack import load_data, attack_experiment, membership_inference, train_target_model
 import numpy as np
-import tensorflow as tf
 from sklearn.metrics import roc_curve
 
-def attack_and_train_abridged_data(dataset, members_to_exclude=[],
-                                   acceptable_fpr=0.01):
+def attack_and_train_abridged_data(dataset, attack, members_to_exclude=[]):
     """
     Trains a target model, then performs a Shokri attack to see who was
     exposed
 
     :param tuple dataset: train_x, train_y, test_x, test_y
+    :param str attack: use Shokri or Yeom attack to determine exposure
     :param list members_to_exclude: indices to skip for training
     :param float acceptable_fpr: false positive rate that can tolerated
     :returns: dict containing exposed members, and training and testing
               accuracy
     """
+    assert attack in ['Yeom', 'Shokri'], "Invalid attack type"
     train_x, train_y, test_x, test_y = dataset
     # `mask` has the same length as the training data and is True for rows
     # we want to keep in `abridged_dataset` and False for rows we will ignore
@@ -27,8 +26,8 @@ def attack_and_train_abridged_data(dataset, members_to_exclude=[],
     del train_x
     del train_y
 
-    # train target model on abridged dataset
-    classifier, _, _, _, train_acc, test_acc = train_private(
+    attack_test_x, attack_test_y, test_classes, train_loss, _, train_acc, test_acc \
+    = train_target_model(
             (abridged_train_x, abridged_train_y, test_x, test_y),
             n_hidden=256,
             epochs=100,
@@ -36,58 +35,52 @@ def attack_and_train_abridged_data(dataset, members_to_exclude=[],
             batch_size=200,
             model='softmax',
             l2_ratio=1e-7,
-            silent=False,
-            privacy='grad_pert',
-            dp='rdp',
-            epsilon=50.0,
-            delta=1e-4)
+            save=False,
+            privacy='no_privacy')
 
-    # predictions on data used in training (members)
-    _, target_model_train_predictions = get_predictions(classifier.predict(
-        input_fn=tf.estimator.inputs.numpy_input_fn(
-            x={'x': abridged_train_x},
-            num_epochs=1,
-            shuffle=False)))
-    
-    # predictions on data not used in training
-    _, target_model_test_predictions = get_predictions(classifier.predict(
-        input_fn=tf.estimator.inputs.numpy_input_fn(
-            x={'x': test_x},
-            num_epochs=1,
-            shuffle=False)))
+    if attack == 'Shokri':
+        global class args:
+            target_epochs = 100
+            target_batch_size = 200
+            target_learning_rate = 0.01
+            n_shadow = 5
+            target_n_hidden = 256
+            target_l2_ratio = 1e-7
+            target_model = 'softmax'
+            save_model = False
+            attack_epochs = 100
+            attack_batch_size = 100
+            attack_learning_rate = 0.01
+            attack_n_hidden = 64
+            attack_l2_ratio = 1e-6
+            attack_model = 'nn'
 
-    attack_test_x = np.vstack(
-            [target_model_train_predictions, target_model_test_predictions]
-            ).astype('float32')
-    # labels for predictions -- members (1) then non-members (0)
-    attack_test_y = np.concatenate(
-            [np.ones(abridged_train_x.shape[0]), np.zeros(test_x.shape[0])]
-                ).astype('int32')
-
-    # output classes that are used in attack_test data
-    test_classes = np.concatenate([abridged_train_y, test_y])
-    
-    class args:
-        target_epochs = 100
-        target_batch_size = 200
-        target_learning_rate = 0.01
-        n_shadow = 5
-        target_n_hidden = 256
-        target_l2_ratio = 1e-7
-        target_model = 'softmax'
-        save_model = False
-        attack_epochs = 100
-        attack_batch_size = 100
-        attack_learning_rate = 0.01
-        attack_n_hidden = 64
-        attack_l2_ratio = 1e-6
-        attack_model = 'nn'
-
-    _, attack_pred = attack_experiment(attack_test_x, attack_test_y,
-            test_classes, args)
-    revealed_members_list = revealed_members(attack_test_y,
-                                             attack_pred[:,1],
-                                             acceptable_fpr)
+        _, attack_pred = attack_experiment(attack_test_x, attack_test_y,
+                test_classes, args)
+        revealed_members_list = revealed_members(
+                attack_test_y,
+                attack_pred,
+                threshold=0.5)
+        n_fp = len(false_positives(attack_test_y, classify(attack_pred, 0.5)))
+    elif attack == 'Yeom':
+        train_loss *= 0.01
+        _, mem_pred = membership_inference(
+                np.append(abridged_train_y, test_y),
+                attack_test_x,
+                attack_test_y,
+                train_loss)
+        revealed_members_list = revealed_members(
+                attack_test_y,
+                mem_pred,
+                threshold=train_loss,
+                below_threshold_positive=True)
+        n_fp = len(false_positives(attack_test_y,
+            classify(mem_pred, train_loss, below_threshold_positive=True)))
+    n_tp = len(revealed_members_list)
+    if n_tp + n_fp > 0:
+        ppv = n_tp / (n_tp + n_fp)
+    else:
+        ppv = 0.5
     # adjust for missing indices:
     # go through each removed index and 'reinsert' it,
     # starting with the lowest index and working up
@@ -99,59 +92,90 @@ def attack_and_train_abridged_data(dataset, members_to_exclude=[],
                 revealed_members_list[i] += 1
     return {
             'exposed members': revealed_members_list,
+            'positive predictive value': ppv,
             'target model accuracy': {
                 'train': train_acc,
                 'test': test_acc
             }
            }
 
-def revealed_members(membership, prediction, acceptable_fpr):
+def revealed_members(membership, prediction, threshold=None, acceptable_fpr=None,
+        below_threshold_positive=False):
     """
     Determines which members are revealed by membership inference
-
-    :param ndarray membership: binary membership status for each element in
-                               training set
-    :param ndarray prediction: predicted odds of membership determined by
-                               attack method
+    
+    :param ndarray membership: binary membership status for each element in population
+    :param ndarray prediction: chance of membership determined by attack method (not necessarily 0-1)
+    :param float threshold: predictions above threshold will be classified as members
     :param float acceptable_fpr: false positive rate that can tolerated
+                                 (optional way of setting threshold)
+    :param below_threshold_positive: see classify doc string
     :return: list of indices of members that have been revealed
     """
+    if threshold is None:
+        assert acceptable_fpr is not None, "Must specify either `threshold` or `acceptable_fpr`"
+        threshold = threshold_fixed_fpr(membership, prediction, acceptable_fpr)
+    return true_positives(membership,
+            classify(prediction, threshold, below_threshold_positive=below_threshold_positive))
+
+def threshold_fixed_fpr(membership, prediction, acceptable_fpr):
+    """Returns the lowest prediction threshold that yields FPR < acceptable_fpr"""
     fpr, tpr, thresholds = roc_curve(membership, prediction, pos_label=1)
     l = list(filter(lambda x: x < acceptable_fpr, fpr))
     if len(l) == 0:
         print("Error: low acceptable fpr")
         return None
-    threshold = thresholds[len(l)-1]
-    preds = list(map(lambda val: 1 if val >= threshold else 0, prediction))
-    tp = [a*b for a,b in zip(preds,membership)]
-    revealed = list(map(lambda i: i if tp[i] == 1 else None, range(len(tp))))
-    return list(filter(lambda x: x != None, revealed))
+    return thresholds[len(l)-1]
+
+def classify(prediction, threshold, below_threshold_positive):
+    """classify predictions/weights by less than threshold (False) or greater (True)
+       (reversed if below_threshold_positive is true"""
+    if below_threshold_positive:
+        return list(map(lambda val: val <= threshold, prediction))
+    else:
+        return list(map(lambda val: val >= threshold, prediction))
+
+def true_positives(membership, predicted_membership):
+    """List of indices that are correctly predicted as members"""
+    tp_status = [m and p_m for m, p_m in zip(membership, predicted_membership)]
+    return list(np.where(np.array(tp_status))[0])
+
+def false_positives(membership, predicted_membership):
+    """List of indices that are incorrectly predicted as members"""
+    fp_status = [p_m and not m for m, p_m in zip(membership, predicted_membership)]
+    return list(np.where(np.array(fp_status))[0])
 
 if __name__=='__main__':
     dataset = load_data('target_data.npz')
     all_exposed_members = set()
-    while True:
-        result = attack_and_train_abridged_data(dataset,
-                                                list(all_exposed_members))
-        print(result)
+    with open('exposed_members.txt', 'r') as f:
+        all_exposed_members = set([int(m) for m in f.readlines()])
+    runs = 0
 
-        if len(all_exposed_members) > 0:
+    while True:
+        result = attack_and_train_abridged_data(
+                dataset,
+                'Yeom',
+                members_to_exclude=list(all_exposed_members))
+        print(f"Run {runs}")
+        print(result)
+        with open('exposed_members.txt', 'a') as f:
+            for exposed_member in result['exposed members']:
+                print(exposed_member, file=f)
+
+        if runs > 0:
             print(
               (f"After removing {len(all_exposed_members)-last_num_exposed}"
                f" members ({len(all_exposed_members)} total) and retraining,"
-               "there are now"
-               f" {len(result['exposed members'])} members exposed.")
-            )
+               f" there are now"
+               f" {len(result['exposed members'])} members exposed."
+               f" (PPV: {result['positive predictive value']:.2%})")
+              )
             print(
                (f"Test accuracy changed from {last_test_accuracy}"
-                f" to {result['target model accuracy']['test']}.")
-            )
+                f" to {result['target model accuracy']['test']}."))
 
         last_num_exposed = len(all_exposed_members)
         all_exposed_members.update(result['exposed members'])
         last_test_accuracy = result['target model accuracy']['test']
-
-        if len(result['exposed members']) == 0:
-            print((f"After removing {len(all_exposed_members)} members,"
-                    " no more members are exposed."))
-            break
+        runs += 1
